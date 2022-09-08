4e70903366173d74f259f85aef3229c87cf8d00c
nadia <dracken24@gmail.com>
2022-09-08 14:46:39 -0400

    save before tout fucker

--- srcs/engine/engine.c
+++ srcs/engine/engine.c
@@ -6,7 +6,7 @@
 /*   By: nadesjar <dracken24@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/17 11:55:26 by nadesjar          #+#    #+#             */
-/*   Updated: 2022/09/08 11:38:48 by nadesjar         ###   ########.fr       */
+/*   Updated: 2022/09/08 14:37:53 by nadesjar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,13 +50,13 @@ void	ft_redirect_output_append(t_cmd *cmd)
 {
 	char	*str;
 
-	str = get_next_line(cmd->outfile);
+	str = get_next_line(cmd->fd_out);
 	while (str)
 	{
 		if (str)
 			printf("%s", str);
 		free(str);
-		str = get_next_line(cmd->outfile);
+		str = get_next_line(cmd->fd_out);
 	}
 	free(str);
 }
@@ -75,22 +75,22 @@ void	ft_find_redirect(t_data *data, int nb)
 	{
 		if (ft_strncmp(data->cmd[nb].token[i], ">>", 2) == 0)
 		{
-			data->cmd[nb].outfile = ft_open_fd(data->cmd[nb].token[++i], 4);
+			data->cmd[nb].fd_out = ft_open_fd(data->cmd[nb].token[++i], 4);
 			ft_redirect_output_append(&data->cmd[nb]);
-			dup2(data->cmd[nb].outfile, STDOUT_FILENO);
+			dup2(data->cmd[nb].fd_out, STDOUT_FILENO);
 			data->cmd[nb].token[--i] = NULL;
 		}
 		else if (ft_strncmp(data->cmd[nb].token[i], ">", 1) == 0)
 		{
-			data->cmd[nb].outfile = ft_open_fd(data->cmd[nb].token[++i], 2);
-			dup2(data->cmd[nb].outfile, STDOUT_FILENO);
+			data->cmd[nb].fd_out = ft_open_fd(data->cmd[nb].token[++i], 2);
+			dup2(data->cmd[nb].fd_out, STDOUT_FILENO);
 			data->cmd[nb].token[--i] = NULL;
 		}
 		else if (ft_strncmp(data->cmd[nb].token[i], "<", 1) == 0
 			&& data->cmd[nb].token[i][1] != '<')
 		{
-			data->cmd[nb].infile = ft_open_fd(data->cmd[nb].token[++i], 2);
-			dup2(data->cmd[nb].infile, STDIN_FILENO);
+			data->cmd[nb].fd_in = ft_open_fd(data->cmd[nb].token[++i], 2);
+			dup2(data->cmd[nb].fd_in, STDIN_FILENO);
 		}
 		// i = -1;
 		// while (data->cmd[nb].token[++i])
--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: nadesjar <dracken24@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/31 03:31:08 by nadesjar          #+#    #+#             */
-/*   Updated: 2022/09/08 14:05:03 by nadesjar         ###   ########.fr       */
+/*   Updated: 2022/09/08 14:24:05 by nadesjar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -235,9 +235,180 @@ void ft_export(t_data *data, char *arg)
 
 /* **************************PARSING***************************************** */
 
+char	*ft_trim_token(char *buffer, char sep)
+{
+	int		i;
+	if (!buffer)
+		return (buffer);
+	i = ft_strlen(buffer) - 1;
+	while (buffer[i] == sep)
+	{
+		buffer[i] = '\0';
+		i--;
+	}
+	while (*buffer == sep)
+		buffer++;
+	return (buffer);
+}
 
+int	ft_token_count(char *buffer, char sep)
+{
+	char *tmp;
+	char *token;
+	int i;
+	
+	i = 0;
+	tmp = ft_strdup(buffer);
+	token = ft_trim_token(ft_strtok(tmp, sep), ' ');
+	while(token)
+	{
+		i++;
+		token = ft_trim_token(ft_strtok(NULL, sep), ' ');
+	}
+	free(tmp);
+	return (i);
+}
 
+char	*ft_remove_char(char *token, char sep)
+{
+	int i;
+	int j;
+	
+	i = 0;
+	j = 0;
+	while (token[i])
+	{
+		if (token[i] == sep)
+		{
+			j = i;
+			while (token[j])
+			{
+				token[j] = token[j + 1];
+				j++;
+			}
+		}
+		else
+			i++;
+	}
+	return (token);
+}
 
+char	*ft_expand(t_data *data, char *token, int flag)
+{
+	char	*temp[4];
+
+	temp[0] = ft_strchr(token, '$');
+	temp[1] = ft_remove_char(ft_substr(token, 0, temp[0] - token), '\"');
+	temp[2] = temp[0] + 1;
+	while (++temp[0])
+		if (*temp[0] == '\0' || *temp[0] == ' ' || *temp[0] == '$' || *temp[0] == '"' || *temp[0] == '\'')
+			break ;
+	temp[2] = ft_substr(temp[2] , 0, temp[0] - temp[2] );
+	temp[3] = ft_get_variable(data, temp[2] );
+	temp[3]  = ft_strjoin(temp[1], temp[3], 1);
+	free (temp[2]);
+	temp[1] = ft_remove_char(ft_substr(temp[0], 0, ft_strlen(temp[0])), '\"');
+	temp[0] = ft_strjoin(temp[3] , temp[1], 1);
+	free(temp[1]);
+	if (flag == 1)
+		free (token);
+	if (ft_strchr(temp[0], '$'))
+		temp[0] = ft_expand(data, temp[0], 1);
+	return (temp[0]);
+}
+
+char	*ft_expand_variable(t_data *data, char *token)
+{
+	char	*temps;
+	char	*expand;
+
+	if (token[0] == '$' && ft_strchr(&token[1], '$') == NULL)
+		token = ft_get_variable(data, &token[1]);
+	else
+	{
+		if (token[0] == '\"')
+			token = ft_expand(data, token + 1, 0);
+		else
+			token = ft_expand(data, token, 0);
+		expand = ft_strjoin(data->expand, "-expand=", 0);
+		temps = ft_strjoin(expand, token, 0);
+		free(token);
+		ft_export(data, temps);
+		free(temps);
+		expand[ft_strlen(expand) - 1] = '\0';
+		token = ft_get_variable(data, expand);
+		free(expand);
+		data->expand[0] = data->expand[0] + 1; 
+	}
+	return (token);
+}
+
+void	ft_clean_token(t_data *data, char **token)
+{
+	int t;
+
+	t = 0;
+	while (token[t])
+	{
+		if (token[t][0] == '\'' && token[t][ft_strlen(token[t]) - 1] == '\'')
+			ft_remove_char(token[t], '\'');
+		else if (token[t][0] == '\"' && token[t][ft_strlen(token[t]) - 1] == '\"')
+		{
+			if (ft_strchr(token[t], '$'))
+				token[t] = ft_expand_variable(data, token[t]);
+			else
+				ft_remove_char(token[t], '\"');
+		}
+		else
+		{
+			token[t] = ft_trim_token(token[t], ' ');
+			if (ft_strchr(token[t], '$'))
+				token[t] = ft_expand_variable(data, token[t]);
+		}
+		t++;
+	}
+}
+
+void	ft_make_token(t_data *data)
+{
+	int c;
+	int t;
+	int count;
+
+	c = -1;
+	while (++c < data->cmd_count)
+	{
+		count = ft_token_count(data->cmd[c].buffer, ' ');
+		data->cmd[c].token = ft_calloc(sizeof(char *), count + 2);
+		t = 0;
+		data->cmd[c].token[t] = ft_strtok(data->cmd[c].buffer, ' ');
+		while (data->cmd[c].token[t])
+		{
+			t++;
+			data->cmd[c].token[t] = ft_strtok(NULL, ' ');
+		}
+	}
+	// ft_check_heredoc(data);
+	// remplace <<var par <heredoc || << var par < heredoc
+	c = -1;
+	while (++c < data->cmd_count)
+		ft_clean_token(data, data->cmd[c].token);
+}
+
+void 	ft_parse(t_data *data)
+{
+	int i;
+
+	i = 0;
+	data->cmd_count = ft_token_count(data->buffer, '|');
+	data->cmd = ft_calloc(sizeof(t_cmd), data->cmd_count + 1);
+	if (data->cmd == NULL)
+		ft_exit(data, "Malloc error\n", 2);
+	data->cmd[0].buffer = ft_trim_token(ft_strtok(data->buffer, '|'), ' ');
+	while (++i < data->cmd_count)
+		data->cmd[i].buffer = ft_trim_token(ft_strtok(NULL, '|'), ' ');
+	ft_make_token(data);
+}
 
 
 /* **********************ENGINE********************************************** */
