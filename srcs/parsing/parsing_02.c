00f6833589f3e72e21b09ede80c50ab6452d2a46
nadia <dracken24@gmail.com>
2022-09-09 13:35:25 -0400

    ok

--- srcs/parsing/parsing_02.c
+++ srcs/parsing/parsing_02.c
@@ -6,7 +6,7 @@
 /*   By: nadesjar <dracken24@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/17 11:48:32 by nadesjar          #+#    #+#             */
-/*   Updated: 2022/09/08 15:09:24 by nadesjar         ###   ########.fr       */
+/*   Updated: 2022/09/09 13:21:05 by nadesjar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -93,69 +93,3 @@ char	*ft_expand(t_data *data, char *token, int flag)
 		temp[0] = ft_expand(data, temp[0], 1);
 	return (temp[0]);
 }
-
-// void	ft_clean_token(t_data *data, char **token)
-// {
-// 	int	t;
-
-// 	t = 0;
-// 	while (token[t])
-// 	{
-// 		if (token[t][0] == '\'' && token[t][ft_strlen(token[t]) - 1] == '\'')
-// 			ft_remove_char(token[t], '\'');
-// 		else if (token[t][0] == '\"'
-// 			&& token[t][ft_strlen(token[t]) - 1] == '\"')
-// 		{
-// 			if (ft_strchr(token[t], '$'))
-// 				token[t] = ft_expand_variable(data, token[t]);
-// 			else
-// 				ft_remove_char(token[t], '\"');
-// 		}
-// 		else
-// 		{
-// 			token[t] = ft_trim_token(token[t], ' ');
-// 			if (ft_strchr(token[t], '$'))
-// 				token[t] = ft_expand_variable(data, token[t]);
-// 		}
-// 		t++;
-// 	}
-// }
-
-// void	ft_make_token(t_data *data)
-// {
-// 	int	c;
-// 	int	t;
-// 	int	count;
-
-// 	c = -1;
-// 	while (++c < data->cmd_count)
-// 	{
-// 		count = ft_token_count(data->cmd[c].buffer, ' ');
-// 		data->cmd[c].token = ft_calloc(sizeof(char *), count + 2);
-// 		t = 0;
-// 		data->cmd[c].token[t] = ft_strtok(data->cmd[c].buffer, ' ');
-// 		while (data->cmd[c].token[t])
-// 		{
-// 			t++;
-// 			data->cmd[c].token[t] = ft_strtok(NULL, ' ');
-// 		}
-// 	}
-// 	c = -1;
-// 	while (++c < data->cmd_count)
-// 		ft_clean_token(data, data->cmd[c].token);
-// }
-
-// void	ft_parse(t_data *data)
-// {
-// 	int	i;
-
-// 	i = 0;
-// 	data->cmd_count = ft_token_count(data->buffer, '|');
-// 	data->cmd = ft_calloc(sizeof(t_cmd), data->cmd_count + 1);
-// 	if (data->cmd == NULL)
-// 		ft_exit(data, "Malloc error\n", 2);
-// 	data->cmd[0].buffer = ft_trim_token(ft_strtok(data->buffer, '|'), ' ');
-// 	while (++i < data->cmd_count)
-// 		data->cmd[i].buffer = ft_trim_token(ft_strtok(NULL, '|'), ' ');
-// 	ft_make_token(data);
-// }
