40801454160ca7a4f45bede85ba5c358a4aeec47
nadia <dracken24@gmail.com>
2022-09-13 19:55:28 -0400

    ok

--- srcs/kill/exit.c
+++ srcs/kill/exit.c
@@ -6,7 +6,7 @@
 /*   By: nadesjar <dracken24@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/17 11:43:29 by nadesjar          #+#    #+#             */
-/*   Updated: 2022/09/12 15:09:31 by nadesjar         ###   ########.fr       */
+/*   Updated: 2022/09/13 19:48:07 by nadesjar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,22 +36,6 @@ void	ft_exit(char *str, int s)
 		ft_free_table();
 	exit(0);
 }
-
-// void	ft_exit(char *str, int s)
-// {
-// 	if (s <= 0)
-// 		ft_putstr_fd(str, 2);
-// 	if (s <= 1)
-// 		free(data.buffer);
-// 	if (s <= 2)
-// 	{
-// 		ft_free_ptr(data.env);
-// 		free(data.pid);
-// 	}
-// 	if (s <= 3)
-// 		ft_free_table();
-// 	exit(0);
-// }
 
 void	ft_quit(char *str)
 {
--- srcs/utils/keep_history.c
+++ srcs/utils/keep_history.c
@@ -6,7 +6,7 @@
 /*   By: nadesjar <dracken24@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 16:56:21 by nadesjar          #+#    #+#             */
-/*   Updated: 2022/09/13 19:36:33 by nadesjar         ###   ########.fr       */
+/*   Updated: 2022/09/13 19:53:56 by nadesjar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,11 @@ void	ft_moove_history(void)
 	int		i;
 
 	i = 0;
-	tmp = ft_calloc(sizeof(char *), 23);
-	fd = ft_open_fd(ft_get_variable("HISTORY"), 6);
+	tmp = ft_calloc(sizeof(char *), 24);
 	tmp[i] = get_next_line(fd);
 	if (!tmp[i])
-	{
-		close(fd);
 		return ;
-	}
+	fd = ft_open_fd(ft_get_variable("HISTORY"), 6);
 	free(tmp[i]);
 	while (1)
 	{
@@ -59,7 +56,6 @@ bool	ft_refresh_history(int fd)
 		i++;
 		if (i >= 20)
 		{
-			// ft_putstr_fd("\n", fd);
 			close(fd);
 			ft_moove_history();
 			free(tmp);
@@ -68,7 +64,6 @@ bool	ft_refresh_history(int fd)
 		free(tmp);
 		tmp = get_next_line(fd);
 	}
-	// ft_putstr_fd("\n", fd);
 	return (false);
 }
 
@@ -105,87 +100,3 @@ void	ft_add_history(char *name)
 	free(tmp);
 	close(fd);
 }
-
-// void	ft_moove_history(void)
-// {
-// 	char	**tmp;
-// 	int		fd;
-// 	int		i;
-
-// 	i = 0;
-// 	tmp = ft_calloc(sizeof(char *), 24);
-// 	fd = ft_open_fd(ft_get_variable("HISTORY"), 6);
-// 	tmp[i] = get_next_line(fd);
-// 	free(tmp[i]);
-// 	while (1)
-// 	{
-// 		tmp[i] = get_next_line(fd);
-// 		if (!tmp[i])
-// 			break ;
-// 		i++;
-// 	}
-// 	tmp[i] = data.buffer;
-// 	close (fd);
-// 	fd = ft_open_fd("HISTORY", 2);
-// 	i = -1;
-// 	while (tmp[++i])
-// 		ft_putstr_fd(tmp[i], fd);
-// 	close(fd);
-// 	ft_putstr_fd("\n", fd);
-// 	// ft_free_ptr(tmp);
-// }
-
-// bool	ft_refresh_history(int fd)
-// {
-// 	char	*tmp;
-// 	int		i;
-
-// 	i = 0;
-// 	tmp = get_next_line(fd);
-// 	while (tmp)
-// 	{
-// 		i++;
-// 		if (i >= 20)
-// 		{
-// 			ft_putstr_fd("\n", fd);
-// 			close(fd);
-// 			ft_moove_history();
-// 			free(tmp);
-// 			return (true);
-// 		}
-// 		free(tmp);
-// 		tmp = get_next_line(fd);
-// 	}
-// 	free(tmp);
-// 	return (false);
-// }
-
-// void	ft_save_history(void)
-// {
-// 	int		fd;
-
-// 	if (ft_is_only(data.buffer, ' ') == true)
-// 		return ;
-// 	fd = ft_open_fd(ft_get_variable("HISTORY"), 6);
-// 	if(ft_refresh_history(fd) == true)
-// 		return ;
-// 	ft_putstr_fd(data.buffer, fd);
-// 	ft_putstr_fd("\n", fd);
-// 	close(fd);
-// }
-
-// void	ft_add_history(char *name)
-// {
-// 	char	*tmp;
-// 	int		fd;
-
-// 	fd = ft_open_fd(ft_get_variable(name), 4);
-// 	tmp = get_next_line(fd);
-// 	while (tmp)
-// 	{
-// 		add_history(tmp);
-// 		free(tmp);
-// 		tmp = get_next_line(fd);
-// 	}
-// 	close(fd);
-// }
